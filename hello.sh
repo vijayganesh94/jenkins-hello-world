85849b17ef4ef6e278ff73a07af29e9d878c7055
vijayganesh <imvijay594@gmail.com>
2025-08-30 18:46:38 +0530

    Initial commit: adding hello.sh script

---
+++ hello.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# A simple build script for demonstration
+echo "=== Jenkins Build Started ==="
+date
+echo "Listing directory contents:"
+ls -la
+echo "Hello, World from my Jenkins build on AWS EC2!"
+echo "=== Jenkins Build Finished ==="
+
